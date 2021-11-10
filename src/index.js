de64b931866f52bb18de8626ccbe7b7a8aee229b
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-10 14:01:26 -0300

    Added Delete Request Added Get Account Request

--- src/index.js
+++ src/index.js
@@ -89,4 +89,50 @@ app.post('/withdraw', verifyIfCPFExists, (request, response) => {
   return response.status(201).send()
 })
 
+app.get('/statement/date', verifyIfCPFExists, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = customer.statement.filter(
+    statement =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  )
+
+  return response.json(statement)
+})
+
+app.put('/account', verifyIfCPFExists, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
+app.get('/account', verifyIfCPFExists, (request, response) => {
+  const { customer } = request
+
+  return response.json(customer)
+})
+
+app.delete('/account', verifyIfCPFExists, (request, response) => {
+  const { customer } = request
+
+  customers.splice(customers.indexOf(customer), 1)
+
+  return response.status(200).json(customers)
+})
+
+app.get('/balance', verifyIfCPFExists, (request, response) => {
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+})
+
 app.listen(3333)
