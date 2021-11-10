6677b48b211fb485cec32c421cae65edd08908aa
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-10 13:11:27 -0300

    Added Deposit Request

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfCPFExists(request, response, next) {
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' })
+  }
+  request.customer = customer
+
+  return next()
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
 
@@ -25,16 +38,25 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers
-
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' })
-  }
-
+app.get('/statement', verifyIfCPFExists, (request, response) => {
+  const { customer } = request
   return response.json(customer.statement)
+})
+
+app.post('/deposit', verifyIfCPFExists, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
 })
 
 app.listen(3333)
